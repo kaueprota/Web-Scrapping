15da43d9e37fb27cac77f3390032c981bdff3337
Lucas Rodrigues <lukinhasr1423@gmail.com>
2023-05-18 18:22:35 -0300

    Adicionado as validações. Fixes #4

--- src/main/java/fiap/idwall/API/model/Wanted.java
+++ src/main/java/fiap/idwall/API/model/Wanted.java
@@ -1,6 +1,8 @@
 package fiap.idwall.API.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -17,17 +19,28 @@ public class Wanted {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String img;
+
+    @NotBlank
     private String name;
     private int age;
+    @Size(max = 10)
     private String Hair;
+    @Size(max = 20)
     private String Eyes;
+    @Size(max = 4)
     private String Height;
     private Date date_of_birth;
+    @Size(max = 2)
     private String sex;
     private String citizenship;
+
     @Enumerated(EnumType.STRING)
+    @NotBlank
     private WantedFor wantedFor;
+
     @Enumerated(EnumType.STRING)
+    @NotBlank
     private Category category;
+
     private double reward;
 }
